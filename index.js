c12c1d239cf49b5dbd992bc231cba46c817094c3
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-02-09 17:38:22 +0100

    Added JS functionality for score +1

---
+++ index.js
@@ -0,0 +1,21 @@
+let homeScoreEl = document.getElementById("home-score-el");
+let guestScoreEl = document.getElementById("guest-score-el");
+
+let homeScore = 0;
+let guestScore = 0;
+
+function addOnePointHome() {
+  homeScore += 1;
+  homeScoreEl.innerText = homeScore;
+}
+
+function addTwoPointHome() {
+  homeScore += 2;
+  homeScoreEl.innerText = homeScore;
+}
+
+let homeAddOne = document.getElementById("home-add-one");
+homeAddOne.addEventListener("click", addOnePointHome);
+
+let homeAddTwo = document.getElementById("home-add-one");
+homeAddOne.addEventListener("click", addTwoPointHome);
