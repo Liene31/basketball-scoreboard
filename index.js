059104720359504c3c9cc821301034b7d531c758
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-02-10 18:05:37 +0100

    Added Functionality for Guest score

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ let guestScoreEl = document.getElementById("guest-score-el");
 let homeScore = 0;
 let guestScore = 0;
 
+//HOME
+
 function addOnePointHome() {
   homeScore += 1;
   homeScoreEl.innerText = homeScore;
@@ -27,3 +29,29 @@ homeAddTwo.addEventListener("click", addTwoPointHome);
 
 let homeAddThree = document.getElementById("home-add-three");
 homeAddThree.addEventListener("click", addThreePointHome);
+
+//GUEST
+
+function addOnePointGuest() {
+  guestScore += 1;
+  guestScoreEl.innerText = guestScore;
+}
+
+function addTwoPointGuest() {
+  guestScore += 2;
+  guestScoreEl.innerText = guestScore;
+}
+
+function addThreePointGuest() {
+  guestScore += 3;
+  guestScoreEl.innerText = guestScore;
+}
+
+let guestAddOne = document.getElementById("guest-add-one");
+guestAddOne.addEventListener("click", addOnePointGuest);
+
+let guestAddTwo = document.getElementById("guest-add-two");
+guestAddTwo.addEventListener("click", addTwoPointGuest);
+
+let guestAddThree = document.getElementById("guest-add-three");
+guestAddThree.addEventListener("click", addThreePointGuest);
